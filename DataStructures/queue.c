100fc7e286a9e0e30bb5daebb3b5f13671b21594
sinarhen-knu <105736826+sinarhen@users.noreply.github.com>
2024-04-05 15:33:34 +0300

    queue implementation c

---
+++ DataStructures/queue.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Queue {
+    int head, tail, size;
+    unsigned capacity;
+    int* array;
+};
+
+struct Stack {
+    int top;
+    unsigned capacity;
+    int* array;
+};
+
+struct Queue* createQueue(unsigned capacity) {
+    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
+    queue->capacity = capacity;
+    queue->head = 0;
+    queue->size = 0;
+    queue->tail = capacity - 1;
+    queue->array = (int*)malloc(queue->capacity * sizeof(int));
+    
+    return queue;
+}
+
+void enqueue(struct Queue* queue, int value) {
+    printf("Enqueuing %d value in queue\n", value);
+    if (queue->size == queue-> capacity) return;
+
+    queue->tail = (queue -> tail + 1)%queue->capacity;
+    queue -> size += 1;
+    queue->array[queue->tail] = value;   
+}
+
+int dequeue(struct Queue* queue){
+    if (queue-> size == 0){
+        return -1;
+    }
+    int value = queue->array[queue -> head];
+    queue -> head = (queue -> head + 1) % queue -> capacity;
+    queue -> size = queue -> size - 1;
+    return value ;
+}
+
+void printQueue(struct Queue* queue){
+    for(int i = 0; i < queue -> size; i++){
+        printf("%d-th queued element is a %d\n", i, queue->array[(queue->head + i)%queue->capacity]);
+    }
+}
+
+void freeQueue(struct Queue* queue) {
+    free(queue->array);
+    free(queue);
+}
+
+int main() {
+    int size = 10;
+    int arr[size] = {32, 23, 55, 34, 56, 95, 21, 19, 99, 43}; 
+
+    struct Queue* queue = createQueue(size);
+    for (int i = 0; i < size; i++){
+        enqueue(queue, arr[i]);
+    }
+
+
+    int lastDequeued = dequeue(queue);
+    if (lastDequeued == -1) return - 1;
+    printf("first element in queue is  %d\n", lastDequeued);
+    printQueue(queue);
+
+    freeQueue(queue);
+    
+    return 0;
+}
--- Module1/testSort.py
+++ Module1/testSort.py
@@ -1,7 +1,7 @@
 
 
 
-test_array = [i for i in range(100, 0, -1)]  
+test_array = [i for i in range(6, 0, -1)]  
 
 
 
@@ -15,47 +15,80 @@ def insertion_sort(array):
             while (j > 0 and array[j] < array[j - 1]):
                 array[j], array[j - 1] = array[j - 1], array[j]
                 j -= 1
-
     return array
 
 
+
 def mergeSort(arr, left, right):
-    if (left >= right):
+    if (right <= left):
         return
-    if right - left == 1:
-        if arr[right] < arr[left]:
+    if (right == left + 1):
+        if (arr[left] > arr[right]):
             arr[left], arr[right] = arr[right], arr[left]
         return
-    else:
-        mid = (left + right) // 2
-        mergeSort(arr, left, mid)
-        mergeSort(arr, mid + 1, right)
-        merge(arr, left, mid, right)
+    mid = (left + right) // 2
+    mergeSort(arr, left, mid)
+    mergeSort(arr, mid + 1, right)
+    merge(arr, left, mid, right)
+
+
+
+
+
+
+
+
+
+
+
+
+# def mergeSort(arr, left, right):
+#     if (left >= right):
+#         return
+#     if right - left == 1:
+#         if arr[right] < arr[left]:
+#             arr[left], arr[right] = arr[right], arr[left]
+#         return
+#     else:
+#         mid = (left + right) // 2
+#         mergeSort(arr, left, mid)
+#         mergeSort(arr, mid + 1, right)
+#         merge(arr, left, mid, right)
 
 
 def merge(arr, left, mid, right):
     i = left
     j = mid + 1
     tmp = []
-    while (i <= mid and j <= right):
+    print("First subarray: ", arr[i:mid+1])
+    print("Second subarray: ", arr[j:right + 1])
+    while i <= mid and j <= right:
         if arr[i] < arr[j]:
+            print(f"Comparing {arr[i]} from first subarray and {arr[j]} from second subarray.")
+            print(f"{arr[i]} is smaller, so add it to the temporary array.")
             tmp.append(arr[i])
             i += 1
         else:
+            print(f"Comparing {arr[i]} from first subarray and {arr[j]} from second subarray.")
+            print(f"{arr[j]} is smaller, so add it to the temporary array.")
             tmp.append(arr[j])
             j += 1
-        
+        print("Temporary array after this step: ", tmp)
     while i <= mid:
+        print(f"All elements from second subarray are added. Add remaining elements from first subarray: {arr[i]}")
         tmp.append(arr[i])
         i += 1
     while j <= right:
+        print(f"All elements from first subarray are added. Add remaining elements from second subarray: {arr[j]}")
         tmp.append(arr[j])
         j += 1
-         
+    print("Temporary array after adding remaining elements: ", tmp)
     for k in range(right - left + 1):
         arr[left + k] = tmp[k]
-
+    print("Final sorted array for this merge step: ", arr[left:right+1])
 
 
 mergeSort(test_array, 0, len(test_array) - 1)
 print(test_array)
+
+
