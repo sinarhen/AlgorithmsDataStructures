a4389ec96e37a862e328a61d82022a89072734b3
sinarhen-knu <105736826+sinarhen@users.noreply.github.com>
2024-04-07 15:43:00 +0300

    Heap basic operations

--- DataStructures/heap.c
+++ DataStructures/heap.c
@@ -5,66 +5,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-struct HeapKey{
-    int key;
-    int right;
-    int left;
-};
-typedef struct HeapKey HeapKey;
-
 struct Heap {
-    HeapKey* array;
+    int* array;
     int capacity;
     int size;
 };
 
 typedef struct Heap Heap;
 
-Heap* createHeap(int capacity, int* arr){
-    Heap* heap = (Heap*)malloc(sizeof(Heap));
+Heap* createHeap(int capacity, const int* arr) {
+    Heap *heap = (Heap *) malloc(sizeof(Heap));
     heap->capacity = capacity;
     heap->size = 0;
-    heap->array = (HeapKey*)malloc(sizeof(HeapKey) * capacity);
+    heap->array = (int *) malloc(sizeof(int) * capacity);
 
     for (int i = 0; i < capacity; i++){
-        heap->array[i].key = arr[i];
-        if (2*i + 1 < capacity) {
-            heap->array[i].left = 2*i + 1;
-        } else {
-            heap->array[i].left = -1;
-        }
-        if (2*i + 2 < capacity) {
-            heap->array[i].right = 2*i + 2;
-        } else {
-            heap->array[i].right = -1;
-        }
+        heap->array[i] = arr[i];
+        heap->size += 1;
     }
-    heap->size = capacity;
-
     return heap;
 }
+
+
+void swap(int* arr, int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+void bubbleUpMaxHeap(Heap* heap, int i){
+    if (i <= 0){
+        return;
+    }
+    if (heap->array[i] > heap->array[(i - 1) / 2]){
+        swap(heap->array, i, (i - 1) / 2);
+        bubbleUpMaxHeap(heap, (i - 1) / 2);
+    }
+}
+
+void bubbleDownMaxHeap(Heap* heap, int i){
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    int largest = i;
+    if (left < heap->size && heap->array[left] > heap->array[largest]){
+        largest = left;
+    }
+    if (right < heap->size && heap->array[right] > heap->array[largest]){
+        largest = right;
+    }
+    if (largest != i){
+        swap(heap->array, i, largest);
+        bubbleDownMaxHeap(heap, largest);
+    }
+}
+
+void bubbleDownMinHeap(Heap* heap, int i){
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    int smallest = i;
+    if (left < heap->size && heap->array[left] < heap->array[smallest]){
+        smallest = left;
+    }
+    if (right < heap->size && heap->array[right] < heap->array[smallest]){
+        smallest = right;
+    }
+    if (smallest != i){
+        swap(heap->array, i, smallest);
+        bubbleDownMinHeap(heap, smallest);
+    }
+
+}
+
+void bubbleUpMinHeap(Heap* heap, int i){
+    if (i<=0){
+        return;
+    }
+    if (heap->array[i] < heap->array[i / 2]){
+        swap(heap->array, i, i / 2);
+        bubbleUpMinHeap(heap, i / 2);
+    }
+}
+
+
 void minHeapify(Heap* heap){
-    int i = 0;
-    while (i < heap->size/2){
-        int left = 2*i;
-        int right = 2*i + 1;
-        int smallest = i;
-        if (heap->array[left].key < heap->array[i].key){
-            smallest = left;
-        }
-        if (heap->array[right].key < heap->array[smallest].key){
-            smallest = right;
-        }
-        if (smallest != i){
-            HeapKey temp = heap->array[i];
-            heap->array[i] = heap->array[smallest];
-            heap->array[smallest] = temp;
-            i = smallest;
-        } else {
-            break;
-        }
-        i++;
+    for (int i = heap->size / 2 - 1; i >= 0 ; i--){
+        bubbleDownMinHeap(heap, i);
     }
 }
 
@@ -73,21 +97,53 @@ void freeHeap(Heap* heap){
     free(heap);
 }
 
+void printHeapArray(Heap* heap){
+    printf("{ ");
+
+    for (int i = 0; i < heap->size; i++){
+        printf("%d", heap->array[i]);
+        if (i != heap->size - 1){
+            printf(",");
+        }
+    }
+
+    printf(" }\n");
+}
+
+
+void printHeap(Heap* heap){
+
+    for (int i = 0; i < heap->size; i++){
+        printf("Heap key %d\n", heap->array[i]);
+        if (i < heap->size / 2){
+            printf("\tHeap left %d\n", heap->array[i*2]);
+        }
+        if (i < heap->size / 2 - 1){
+            printf("\tHeap right %d\n", heap->array[i * 2 + 1]);
+        }
+
+    }
+}
+
+
+
 int main (){
-    int size = 10;
-    int arr[size];
-    for (int i = 0; i < size; i++){
-        arr[i] = i;
-    }
+    const int size = 10;
+    int arr[10] = {10, 23, 43, 22, 11, 100, 55, 62, 1, 3};
+
     Heap* heap = createHeap(size, arr);
-//    minHeapify(heap);
+    printf("Initial heap:\n");
+    printHeapArray(heap);
 
-    for (int i = 0; i < size; i++){
-        printf("Heap key %d\n", heap->array[i].key);
-        printf("Heap left %d\n", heap->array[i].left);
-        printf("Heap right %d\n", heap->array[i].right);
+    for (int i = heap->size / 2 - 1; i >= 0 ; i--){
+        bubbleDownMaxHeap(heap, i);
     }
+    printf("Heap after maxHeapify:\n");
+    printHeapArray(heap);
 
+    bubbleUpMaxHeap(heap, 9);
+    printf("Heap after bubbleUp [9]:\n");
+    printHeapArray(heap);
 
     freeHeap(heap);
     return 0;
