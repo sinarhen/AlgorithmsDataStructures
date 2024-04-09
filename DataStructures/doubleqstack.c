f5301ed5c8382026e99b4afb28fd37d2ed8532d2
sinarhen-knu <105736826+sinarhen@users.noreply.github.com>
2024-04-09 23:54:54 +0300

    Double q stack

--- DataStructures/doubleqstack.c
+++ DataStructures/doubleqstack.c
@@ -1,5 +1,40 @@
-//
-// Created by Bohdan on 09.04.2024.
-//
-
 #include "doubleqstack.h"
+#include <stdio.h>
+
+
+// 1. Create a DoubleQStack
+// 2. Створений DoubleQStack має дві черги з однаковими розмірами
+// 3. Щоб додати елементи до стеку нам необхідно вибрати одну з черг і додати елемент в кінець черги
+// 4. Після того як ми додали елемент до черги, ми повинні видалити всі елементи з іншої черги і додати їх до першої черги. Це робиться для того аби черга повертала
+// 5. Якщо ми хочемо видалити елемент зі стеку, ми повинні видалити елемент з черги, яка містить елементи стеку
+// 6. Якщо ми хочемо додати елемент до стеку, ми повинні додати елемент до черги, яка містить елементи стеку
+
+int main(){
+    int size = 10;
+    int arr[size];
+
+    printf("Initializing array...\n");
+    for (int i = 0; i < size; i++){
+        arr[i] = i;
+        printf("Added %d to array\n", i);
+    }
+
+    printf("Creating DoubleQStack...\n");
+    DoubleQStack* stack = createDoubleQStack(size);
+
+    printf("Pushing elements to stack...\n");
+    for (int i = 0; i < size; i++){
+        pushDoubleQStack(stack, arr[i]);
+        printf("Pushed %d to stack\n", arr[i]);
+    }
+
+    printf("Popping element from stack...\n");
+    int popped = popDoubleQStack(stack);
+    printf("Popped %d from stack\n", popped);
+
+    printf("Freeing DoubleQStack...\n");
+    freeDoubleQStack(stack);
+
+    printf("Process finished successfully.\n");
+    return 0;
+}
--- DataStructures/doubleqstack.h
+++ DataStructures/doubleqstack.h
@@ -5,4 +5,44 @@
 #ifndef COLORADO_DOUBLEQSTACK_H
 #define COLORADO_DOUBLEQSTACK_H
 
+#include "queue.h"
+
+struct DoubleQStack {
+    Queue* q1;
+    Queue* q2;
+};
+
+typedef struct DoubleQStack DoubleQStack;
+
+DoubleQStack* createDoubleQStack(unsigned capacity) {
+    DoubleQStack* stack = (DoubleQStack*)malloc(sizeof(DoubleQStack));
+    stack->q1 = createQueue(capacity);
+    stack->q2 = createQueue(capacity);
+    return stack;
+}
+
+void pushDoubleQStack(DoubleQStack* stack, int value) {
+    if (stack->q1->size == stack->q1->capacity) return;
+    enqueue(stack->q1, value);
+}
+
+int popDoubleQStack(DoubleQStack* stack) {
+    if (stack->q1->size == 0) return -1;
+    while (stack->q1->size != 1) {
+        enqueue(stack->q2, dequeue(stack->q1));
+    }
+    int value = dequeue(stack->q1);
+    Queue* temp = stack->q1;
+    stack->q1 = stack->q2;
+    stack->q2 = temp;
+    return value;
+}
+
+void freeDoubleQStack(DoubleQStack* stack) {
+    freeQueue(stack->q1);
+    freeQueue(stack->q2);
+    free(stack);
+}
+
+
 #endif //COLORADO_DOUBLEQSTACK_H
