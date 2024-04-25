2b36cd93a096f62ec6ef8e5e30b5f0b523447964
sinarhen-knu <105736826+sinarhen@users.noreply.github.com>
2024-04-25 23:17:24 +0300

    quick select

---
+++ SelectionAlgorithms/quick_select.py
@@ -0,0 +1,32 @@
+
+def partition(arr, l, r):
+    pivot = arr[r]
+    i = l
+    for j in range(l, r):
+        if arr[j] < pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[i], arr[r] = arr[r], arr[i]
+    return i
+
+def kthSmallest(arr, l, r, k): 
+  
+    if (k > 0 and k <= r - l + 1): 
+  
+        index = partition(arr, l, r) 
+  
+        if (index - l == k - 1): 
+            return arr[index] 
+  
+        if (index - l > k - 1): 
+            return kthSmallest(arr, l, index - 1, k) 
+  
+        return kthSmallest(arr, index + 1, r,  
+                            k - index + l - 1) 
+    print("Index out of bound") 
+arr = [ 10, 4, 5, 8, 6, 11, 26 ] 
+n = len(arr) 
+k = 3
+print("K-th smallest element is ", end = "") 
+print(kthSmallest(arr, 0, n - 1, k)) 
+  
